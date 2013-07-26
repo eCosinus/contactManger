5b67c60a97cadf9a1fd2a604d213933ae2f95c41
OpenShift guest <cosinusweb@gmail.com>
2013-07-26 07:39:50 -0400

    Contact manager working

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,27 @@
 var app = angular.module("myApp", []);
 
-app.controller('mainCtrl', ['$scope', function($scope) {
+app.controller('editCtrl', ['$scope','$routeParams','$location',function($scope,$routeParams,$location){
+    var newContact = false;
+    if($routeParams.contactId){
+        $scope.contact = $scope.contacts[$routeParams.contactId];
+    } else {
+        $scope.contact = {};
+        newContact = true;
+    }
+    
+    $scope.saveContact = function() {
+        if (newContact) {
+            $scope.contacts.push($scope.contact);
+        }
+        $location.path("/list");
+    }
+    
+}]);
+
+app.controller('listCtrl', ['$scope',function($scope){
+    
+}]);
+app.controller('ContactCtrl', ['$scope', function($scope) {
     $scope.contact = {};
     $scope.contact = [{
         name: 'Fabien',
@@ -22,4 +43,20 @@ app.controller('mainCtrl', ['$scope', function($scope) {
     }];
 
 
+}]);
+
+app.config(['$routeProvider',function($routeProvider){
+    $routeProvider.when('/list',{templateUrl:'partials/list.html',controller:'listCtrl'});
+    $routeProvider.when('/edit/:contactId',{templateUrl:'partials/edit.html',controller:'editCtrl'})
+    $routeProvider.otherwise({redirectTo:'/list'})
 }])
+app.directive('contact', function(){
+   return {
+       restrict:'E',
+       replace:true,
+       templateUrl:'templates/contactWidget.html',
+       scope:{
+           contact: '='
+       }
+   } 
+});
